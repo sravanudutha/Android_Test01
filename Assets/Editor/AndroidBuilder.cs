f7d6d7a7f016bd695b0c805e3c41fe15aa43a131
Sravan Kumar <sravank387@gmail.com>
2020-07-25 15:49:56 +0530

    build script

---
+++ Assets/Editor/AndroidBuilder.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+class AndroidBuilder
+{
+    static void build()
+    {
+
+        // Place all your scenes here
+        string[] scenes = {"./Assets/Scene.unity",
+                            //"Assets/scenes/S_Login.unity",
+                           // "Assets/scenes/S_Help.unity",
+                           // "Assets/scenes/S_1.unity",
+                           // "Assets/scenes/S_Reward.unity",
+                           // "Assets/scenes/S_Credits.unity",
+                           // "Assets/scenes/S_Settings.unity",
+                            };
+
+        string pathToDeploy = ".";
+
+        BuildPipeline.BuildPlayer(scenes, pathToDeploy, BuildTarget.Android, BuildOptions.None);
+    }
+}
